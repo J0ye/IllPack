92eaefca9b7f81ec4cd674c01a84b41cbfbf3fe8
J0ye <joyemueller@aol.com>
2021-09-16 16:00:26 +0200

    Added xr precision grab

--- StateMachine/State.cs
+++ StateMachine/State.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-[CreateAssetMenu(fileName = "New State", menuName = "State Machine/Create New State", order = 1)]
 public class State : MonoBehaviour
 {
     public UnityEvent OnStateStart = new UnityEvent();
