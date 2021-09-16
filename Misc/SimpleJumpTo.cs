5c9734b5d6af820a870935cd6cf214ae8615103e
J0ye <joyemueller@aol.com>
2021-09-16 15:26:52 +0200

    Modified jump

--- Misc/SimpleJumpTo.cs
+++ Misc/SimpleJumpTo.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using System;
+using UnityEngine.Events;
 
 public class SimpleJumpTo : MonoBehaviour
 {
+    [Header("Jump Options")]
     public Transform target;
     public int jumpCount = 1;
     public float jumpPower = 1f;
     public float duration = 1f;
+
+    public UnityEvent OnStartJump = new UnityEvent();
+    public UnityEvent OnFinishJump = new UnityEvent();
 
     public void PlayAnimation()
     {
 
         ResetRigidbody();
         transform.DOJump(target.position, jumpPower, jumpCount, duration, false);
+        OnStartJump.Invoke();
     }
 
     private IEnumerator ResetRigidbody()
@@ -26,5 +32,6 @@ public class SimpleJumpTo : MonoBehaviour
         {
             rb.velocity = Vector3.zero;
         }
+        OnFinishJump.Invoke();
     }
 }
